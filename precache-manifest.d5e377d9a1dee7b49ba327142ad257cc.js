38ca4a67b28635a0d1b6a82eafa30002e4516356
j- <j@skeoh.com>
2020-08-10 00:29:46 +1000

    Updates

---
+++ precache-manifest.d5e377d9a1dee7b49ba327142ad257cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a2dbd94fc0526ca687532f0d260335",
+    "url": "/embiggen/index.html"
+  },
+  {
+    "revision": "1e3952a14c6c0d41f450",
+    "url": "/embiggen/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ee1cd3dc06c49f4762b7",
+    "url": "/embiggen/static/css/main.912802e7.chunk.css"
+  },
+  {
+    "revision": "1e3952a14c6c0d41f450",
+    "url": "/embiggen/static/js/2.03d748c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/embiggen/static/js/2.03d748c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1cd3dc06c49f4762b7",
+    "url": "/embiggen/static/js/main.0f16e314.chunk.js"
+  },
+  {
+    "revision": "273f413cfe9dfe4b3c08",
+    "url": "/embiggen/static/js/runtime-main.cbe3cec5.js"
+  }
+]);
